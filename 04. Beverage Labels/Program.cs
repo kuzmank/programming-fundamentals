297ad42386fe311feb7b5619f289e968713b4622
Svetloslav15 <sn_novoselski@abv.bg>
2018-03-11 12:32:02 +0200

    Add Problem 4 and 5

---
+++ 04. Beverage Labels/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04._Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int sugar = int.Parse(Console.ReadLine());
+            double allSugar = volume * sugar / 100.0;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{volume * energy / 100.0}kcal, {allSugar}g sugars");
+        }
+    }
+}
