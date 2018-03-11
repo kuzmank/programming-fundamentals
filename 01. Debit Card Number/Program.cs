de358600c9cc11b3b436f83343e11eb46c14990a
Svetloslav15 <sn_novoselski@abv.bg>
2018-03-11 11:11:27 +0200

    Added Problem 1

---
+++ 01. Debit Card Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{a:D4} {b:D4} {c:D4} {d:D4}");
+        }
+    }
+}
